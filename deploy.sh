e4ceefece11576e12019a8a99fea30ecd29c6fa7
Aviv Beeri <avbeeri@gmail.com>
2023-03-12 08:34:51 +0000

    deploytweaks

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#
-#CONTENT=$(jq -r ".version=\"$1\"" config.json)
-#echo $CONTENT > config.json
 jq -M ".version=\"$1\"" config.json > config.swap.json
 cat config.swap.json > config.json
 rm -f config.swap.json
 
-dome nest -c res *.wren config.json 
+dome nest -c res *.wren
 mv game.egg ../dome-builds/acolytes-pledge
+cp config.json ../dome-builds/acolytes-pledge
 cd ../dome-builds/acolytes-pledge
 ./upload-all.sh $1 $2
